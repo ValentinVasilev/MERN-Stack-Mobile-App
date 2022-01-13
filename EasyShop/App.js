d0695d93a0c32f87d36e45f107264502b5d42022
ValentinVasilev <falioVasilev@gmail.com>
2022-01-13 18:03:20 +0200

    Intro to Folder Structure

--- EasyShop/App.js
+++ EasyShop/App.js
@@ -29,6 +29,7 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 import TodoList from './components/todoList';
 import Todo from './components/Todo';
+import ProductContainer from './screens/Products/ProductContainer';
 
 const Section = ({children, title}): Node => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -72,69 +73,18 @@ const App: () => Node = () => {
     }
   }
   return (
-    <Section>
-      <View>
-        <Text>{title}</Text>
-        <Button title="Change Title" onPress={() => changeTitle(title)} />
-      </View>
-
-      <Section>
-        <View style={{fontSize: 20}}>
-          <Todo name="name - one" />
-          <Todo name="name - Two" />
-          <Todo name="name - Three" />
-          <Text style={{fontSize: 30}}>Test</Text>
-        </View>
-      </Section>
-    </Section>
-
-    // <SafeAreaView style={backgroundStyle}>
-    //   <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-    //   <ScrollView
-    //     contentInsetAdjustmentBehavior="automatic"
-    //     style={backgroundStyle}>
-    //     <Header />
-    //     <View
-    //       style={{
-    //         backgroundColor: isDarkMode ? Colors.black : Colors.white,
-    //       }}>
-    //       <Section title="Step One">
-    //         Edit <Text style={styles.highlight}>App.js</Text> to change this
-    //         screen and then come back to see your edits. Todo: <TodoList />
-    //       </Section>
-    //       <Section title="See Your Changes">
-    //         <ReloadInstructions />
-    //       </Section>
-    //       <Section title="Debug">
-    //         <DebugInstructions />
-    //       </Section>
-    //       <Section title="Learn More">
-    //         Read the docs to discover what to do next
-    //       </Section>
-    //       <LearnMoreLinks />
-    //     </View>
-    //   </ScrollView>
-    //   <TodoList />
-    // </SafeAreaView>
+    <View style={styles.container}>
+      <ProductContainer />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
---
+++ EasyShop/screens/Products/ProductContainer.js
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import React, {useState} from 'react';
+import {View, StyleSheet, ActivityIndicator, Text} from 'react-native';
+
+const ProductContainer = () => {
+  return (
+    <View>
+      <Text>Product Container</Text>
+    </View>
+  );
+};
+
+export default ProductContainer;
