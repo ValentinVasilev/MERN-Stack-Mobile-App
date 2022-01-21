446e62687acd89b64673d96d5b429eef42d2280b
ValentinVasilev <falioVasilev@gmail.com>
2022-01-21 09:57:37 +0200

    Bottom Container

--- EasyShop/screens/Cart/Cart.js
+++ EasyShop/screens/Cart/Cart.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 /* eslint-disable prettier/prettier */
 import React from 'react';
 import {
@@ -16,10 +17,14 @@ import * as actions from '../../Redux/actions/cartActions';
 
 var {height, width} = Dimensions.get('window');
 import {connect} from 'react-redux';
-import {Box, Container, List, Thumbnail} from 'native-base';
+import { Box, Container, List, Thumbnail } from 'native-base';
+
 
 const Cart = props => {
-  //   console.log(props.cartItems[1].product.image);
+  let total = 0;
+  props.cartItems.forEach(cart => {
+    return (total += cart.product.price)
+  });
 
   return (
     <>
@@ -52,9 +57,10 @@ const Cart = props => {
                       justifyContent: 'space-between',
                       width: 200,
                       borderBottomWidth: 0.8,
+                      marginBottom: 25,
                     }}>
                     <Text style={styles.name}>{data.product.name}</Text>
-                    <Text style={styles.price}>${data.product.price}</Text>
+                    <Text style={styles.price}><Text style={{color: 'green'}}>$</Text> {data.product.price}</Text>
                   </View>
                 </View>
               );
@@ -63,10 +69,21 @@ const Cart = props => {
         </Container>
       ) : (
         <Container style={styles.emptyContainer}>
-          <Text>Looks like your cart is empty.</Text>
-          <Text>Add products to your cart to get started.</Text>
+          <Text style={{color: 'black'}}>Looks like your cart is empty.</Text>
+          <Text style={{color: 'black'}}>Add products to your cart to get started.</Text>
         </Container>
       )}
+      <Container style={styles.bottomContainer}>
+        <View>
+        <Text style={{color: 'red', fontSize: 18, paddingRight: 15, fontWeight: 'bold'}}> <Text style={{color: 'black'}}>Total Price:</Text> ${total}</Text>
+        </View>
+        <View style={{ paddingLeft: 15, paddingRight: 15 }}>
+          <Button title="Clear"/>
+        </View>
+          <View style={{ paddingLeft: 15, paddingRight: 15}}>
+          <Button title='Checkout' onPress={() => props.navigation.navigate('CheckOut')}/>
+        </View>
+      </Container>
     </>
   );
 };
@@ -96,15 +113,30 @@ const styles = StyleSheet.create({
   name: {
     paddingTop: 10,
     marginBottom: 25,
+    color: 'black',
+    fontSize: 15,
+    
     // borderBottomWidth: 0.8,
     // width: '50%',
   },
   price: {
     paddingTop: 10,
     marginBottom: 25,
+    color: 'black',
+    fontSize: 15,
+    fontWeight: 'bold'
     // borderBottomWidth: 0.8,
     // alignSelf: 'flex-end',
     // marginLeft: 50,
   },
+  bottomContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignContent: 'space-between',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    paddingBottom: 25,
+  }
 });
 export default connect(mapStateToProps, null)(Cart);
