57809629a0dac7e2c7efeaff40356260baa11ff1
ValentinVasilev <falioVasilev@gmail.com>
2022-01-21 10:19:23 +0200

    Cart Icon

--- EasyShop/Navigators/Main.js
+++ EasyShop/Navigators/Main.js
@@ -4,9 +4,11 @@ import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {View} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import CartNavigator from './CartNavigator';
+import CartIcon from '../shared/CartIcon';
+
 // Stacks
 import HomeNavigator from './HomeNavigator';
+import CartNavigator from './CartNavigator';
 
 const Tab = createBottomTabNavigator();
 
@@ -31,8 +33,11 @@ const Main = () => {
         name="Cart"
         component={CartNavigator}
         options={{
-          tabBarIcon: ({color}) => (
-            <Icon name="shopping-cart" color={color} size={30} />
+          tabBarIcon: ({ color }) => (
+            <View>
+              <Icon name="shopping-cart" color={color} size={30} />
+              <CartIcon/>
+              </View>
           ),
         }}
       />
---
+++ EasyShop/shared/CartIcon.js
@@ -0,0 +1,46 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Badge, Text } from 'native-base';
+
+import { connect } from 'react-redux';
+
+const CartIcon = (props) => {
+    return (
+    <>
+        {props.cartItems.length ? (
+            <Badge style={styles.badge}  rounded="999px">
+                <Text style={styles.text}>{props.cartItems.length}</Text>
+                </Badge>
+    ) : (null)}
+        </>
+        )
+}
+
+const mapStateToProps = state => {
+    const { cartItems } = state;
+    return {
+        cartItems: cartItems
+    }
+}
+
+const styles = StyleSheet.create({
+    badge: {
+        width: 25,
+        position: 'absolute',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignContent: 'center',
+        top: -4,
+        right: -10,
+        backgroundColor: 'red'
+    },
+    text: {
+        fontSize: 12,
+        // width: 100,
+        fontWeight: 'bold'
+    }
+})
+
+export default connect(mapStateToProps)(CartIcon);
