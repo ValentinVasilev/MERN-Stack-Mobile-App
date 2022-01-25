961c2908185d8cc145df367f4c44509b4629c854
ValentinVasilev <falioVasilev@gmail.com>
2022-01-25 18:06:26 +0200

    isEmpty Check

--- EasyShop/Context/reducers/Auth.reducer.js
+++ EasyShop/Context/reducers/Auth.reducer.js
@@ -1,12 +1,13 @@
 /* eslint-disable prettier/prettier */
 import { SET_CURRENT_USER } from "../actions/Auth.actions";
+import isEmpty from "../../assets/common/isEmpty";
 
 export default function (state, action) {
     switch (action.type) {
         case SET_CURRENT_USER:
             return {
                 ...state,
-                isAuthenticated: [], // TODO
+                isAuthenticated: !isEmpty(action.payload),
                 user: action.payload,
                 userProfile: action.userProfile
             };
