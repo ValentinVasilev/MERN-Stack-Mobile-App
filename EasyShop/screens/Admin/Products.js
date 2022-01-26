88cc3fece3590fa97b154865befde70ece802115
ValentinVasilev <falioVasilev@gmail.com>
2022-01-26 13:22:21 +0200

    Product List Setup

--- EasyShop/screens/Admin/Products.js
+++ EasyShop/screens/Admin/Products.js
@@ -1,13 +1,51 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useCallback } from 'react';
+import { View, Text, FlatList, ActivityIndicator, StyleSheet, Dimensions, Button } from 'react-native';
+import { Heading, Input } from 'native-base';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { useFocusEffect } from '@react-navigation/native';
+
+import axios from 'axios';
+import baseURL from '../../assets/common/baseURL';
+import AsyncStorage from '@react-native-community/async-storage';
+
+var { height, width } = Dimensions.get('window');
 
 const Products = (props) => {
-    return (
-        <View>
-            <Text>Products Screen</Text>
-        </View>
-    );
+
+  const [productList, setProductList] = useState();
+  const [productFilter, setProductFilter] = useState();
+  const [loading, setLoading] = useState(true);
+  const [token, setToken] = useState();
+
+  useFocusEffect(
+    useCallback(() => {
+      // Get our token
+      AsyncStorage.getItem('jwt')
+        .then(res => {
+          setToken(res);
+        })
+        .catch(error => console.log(error));
+
+      axios.get(`${baseURL}products`)
+        .then(res => {
+          setProductList(res.data);
+          setProductFilter(res.data);
+          setLoading(false);
+        })
+
+      return () => {
+        setProductList();
+        setProductFilter();
+        setLoading(true);
+      }
+    }, [])
+  );
+  return (
+    <View>
+      <Text>Products Screen</Text>
+    </View>
+  );
 };
 
 export default Products;
